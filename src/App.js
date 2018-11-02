b49fd3783c9c1a4484aa6cb5951644775c87dc0a
liz <elizabeth.reyes@ifrsf.org>
2018-11-01 21:08:43 -0700

    pass latitutde and longitude to console

--- src/App.js
+++ src/App.js
@@ -2,18 +2,54 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+  coordinateSubmit(e){
+    e.preventDefault();
+      const latitude= e.target.elements.latitude.value;
+      const longitude= e.target.elements.longitude.value;
+      console.log(latitude);
+      console.log(longitude);
+  
+  }
+
+  getweatherdata(){
+      fetch('https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/7f875b51f5e01333c06b1d559e43ce9a/43.3104064, -70.99034379999999')
+      .then(
+        function(response) {
+          if (response.status !== 200) {
+            console.log('Looks like there was a problem. Status Code: ' +
+              response.status);
+            return;
+          }
+
+          // Examine the text in the response
+          response.json().then(function(data) {
+            console.log(data);
+          });
+        }
+      )
+      .catch(function(err) {
+        console.log('Fetch Error :-S', err);
+  });
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Comparison App</h1>
-        <form>
+        <form onSubmit={this.coordinateSubmit}>
           <input
             type="text"
-            name="zipCodeOne"
-            placeholder="Enter First Zip Code"
+            name="latitude"
+            placeholder="Enter Latitude"
           />
-          <button>Submit Zip Code</button>
+          <input
+            type="text"
+            name="longitude"
+            placeholder="Enter longitude"
+          />
+          <button>Submit</button>
         </form>
+        <button onClick= {this.getweatherdata}> get data </button>
       </div>
     );
   }
