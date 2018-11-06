3751d21afc6a4785ac94ffa30b4fa94c749c2bec
gerry <gmercado6@gmail.com>
2018-11-05 21:12:20 -0800

    dual entry and display of weather summary

--- src/App.js
+++ src/App.js
@@ -12,13 +12,18 @@ class App extends Component {
     e.preventDefault();
       const latitude= e.target.elements.latitude.value;
       const longitude= e.target.elements.longitude.value;
+      const latitude2= e.target.elements.latitude2.value;
+      const longitude2= e.target.elements.longitude2.value;
       console.log(latitude);
       console.log(longitude);
-  
+      console.log(latitude2);
+      console.log(longitude2);
+      this.getweatherdata(latitude,longitude, "summary1");
+      this.getweatherdata(latitude2,longitude2, "summary2");
   }
 
-  getweatherdata = () => {
-      fetch('https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/7f875b51f5e01333c06b1d559e43ce9a/43.3104064, -70.99034379999999')
+  getweatherdata = (latitude,longitude, stateKey) => {
+      fetch(`https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/7f875b51f5e01333c06b1d559e43ce9a/${latitude},${longitude}`)
       .then(
         response => {
           if (response.status !== 200) {
@@ -30,7 +35,7 @@ class App extends Component {
           // Examine the text in the response
           response.json().then(data => {
             console.log(data);
-            this.setState({ summary: data.daily.summary })
+            this.setState({...this.state, [stateKey]: data.daily.summary})
           });
         }
       )
@@ -44,20 +49,37 @@ class App extends Component {
       <div className="App">
         <h1>Comparison App</h1>
         <form onSubmit={this.coordinateSubmit}>
+          <div>
           <input
             type="text"
             name="latitude"
             placeholder="Enter Latitude"
+            // value="35.6895"
           />
           <input
             type="text"
             name="longitude"
             placeholder="Enter longitude"
+            // value="139.6917"
           />
+          </div><div>
+          <input
+            type="text"
+            name="latitude2"
+            placeholder="Enter Latitude"
+            // value="35.6895"
+          />
+          <input
+            type="text"
+            name="longitude2"
+            placeholder="Enter longitude"
+            // value="139.6917"
+          />
+          </div>
           <button>Submit</button>
         </form>
-        <button onClick= {this.getweatherdata}> get data </button>
-          <DataDisplay data={this.state.summary}></DataDisplay>
+          <DataDisplay data={this.state.summary1}></DataDisplay>
+          <DataDisplay data={this.state.summary2}></DataDisplay>
       </div>
     );
   }
