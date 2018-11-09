b8898dc1cf8516707dc71e0e942175acf8a08108
lizreyeshe <elizabeth.reyes@ifrsf.org>
2018-11-08 20:50:48 -0800

    added many attributes including Date of sunrise and sun set, and other things

--- src/DataDisplay.js
+++ src/DataDisplay.js
@@ -1,7 +1,24 @@
 import React, { Component } from "react";
 
 export default class DataDisplay extends Component {
-  render(){
-    return <h1>{this.props.data}</h1>
+  render() {
+    return (
+      <>
+        {this.props.data.temperature && (
+          <>
+            <h1>{this.props.data.summary}</h1>
+            <h2>the current temperature is {this.props.data.temperature}F</h2>
+            <h2>
+              the current wind speed is {this.props.data.windSpeed}
+              mph
+            </h2>
+            <h2>the current humidity is {this.props.data.humidity * 100}%</h2>
+            <h2>Current chance of rain {this.props.data.rain * 100}</h2>
+            <h2>Sunrise will be at {this.props.data.sunrise}</h2>
+            <h2>Sunset will be at {this.props.data.sunset}</h2>
+          </>
+        )}
+      </>
+    );
   }
 }
